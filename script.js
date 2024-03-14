271a1f88383db1c24d93d011d8a8b51ee38f4793
Nazar_Panyushchyk <npanusik@gmail.com>
2024-03-14 16:52:47 +0200

    add

---
+++ script.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 3, 4, 5]
+
+for (let i = 0; i <= arr.length - 1; i++) {
+	console.log(arr[i]);
+}
+
+let array = [5, 7, 1, 4, 9]
+
+let sum = 0;
+
+for (let i = 0; i <= array.length; i++) {
+    sum += array[i];
+    console.log(sum);
+}
+
+let number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+console.log(Math.max(10, 20, 30, 40, 50, 60, 70, 80, 90, 100));
+
+
